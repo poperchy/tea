6f7013042ca3d542ed161d4e07bf080db7eb8fa4
jjjj <cmbox123@yandex.by>
2021-01-13 17:39:35 +0300

    added about us

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -394,82 +394,63 @@ $(function () {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 "use strict";
 
 (function () {
     const height = (elem) => {
-
         return elem.getBoundingClientRect().height
-
     }
-
     const distance = (elemA, elemB, prop) => {
-
         const sizeA = elemA.getBoundingClientRect()[prop]
         const sizeB = elemB.getBoundingClientRect()[prop]
-
         return sizeB - sizeA
-
     }
-
     const factor = (elemA, elemB, prop) => {
-
         const sizeA = elemA.getBoundingClientRect()[prop]
         const sizeB = elemB.getBoundingClientRect()[prop]
-
         return sizeB / sizeA
-
     }
-
     document.querySelectorAll('.card').forEach((elem) => {
-
         const head = elem.querySelector('.card__head')
         const image = elem.querySelector('.card__image')
         const author = elem.querySelector('.card__author')
         const body = elem.querySelector('.card__body')
         const foot = elem.querySelector('.card__foot')
-
         elem.onmouseenter = () => {
-
             elem.classList.add('hover')
-
             const imageScale = 1 + factor(head, body, 'height')
-            image.style.transform = `scale(${ imageScale })`
-
+            image.style.transform = `scale(${imageScale})`
             const bodyDistance = height(foot) * 1
-            body.style.transform = `translateY(${ bodyDistance }px)`
-
+            body.style.transform = `translateY(${bodyDistance}px)`
             const authorDistance = distance(head, author, 'height')
-            author.style.transform = `translateY(${ authorDistance }px)`
-
+            author.style.transform = `translateY(${authorDistance}px)`
         }
-
         elem.onmouseleave = () => {
-
             elem.classList.remove('hover')
-
             image.style.transform = `none`
             body.style.transform = `none`
             author.style.transform = `none`
-
         }
-
     })
+
+
+    var acc = document.getElementsByClassName("show-more");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+
+
 })();
 
 
